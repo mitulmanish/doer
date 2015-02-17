c05688374eef915cf73d8fa0bfcb432ae118d780
mitulmanish <mitul.manish@gmail.com>
2015-02-18 01:51:29 +0530

    added style

---
+++ app/controllers/todo_items_controller.rb
@@ -0,0 +1,34 @@
+class TodoItemsController < ApplicationController
+  before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
+  def create
+  @todo_item = @todo_list.todo_items.create(todo_items_params)
+  redirect_to @todo_list
+  end
+  def destroy
+  
+    if @todo_item.destroy
+      flash[:success] = "Item was deleted"
+    else
+      flash[:error] = "Item could not be deleted"
+    end
+    redirect_to @todo_list
+  end
+  
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo Item Completed"
+  end
+  private
+  def set_todo_list
+  @todo_list = TodoList.find(params[:todo_list_id])
+  end
+  
+  def todo_items_params
+  params[:todo_item].permit(:content)
+  end
+  
+  def set_todo_item
+      @todo_item = @todo_list.todo_items.find(params[:id])
+  end
+end
